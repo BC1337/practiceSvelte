a3ff814d4a1183dd9ffd05e6c61272cf26d7a186
BC1337 <brettcomrie@gmail.com>
2023-07-18 15:07:36 -0500

    First look at svelte, seems really the most userfriendly so far.

---
+++ practice1/src/AddPersonForm.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    let name
+    let beltColor
+    let age
+    let skills = [];
+
+    const handleSubmit = () => {
+        const person = {
+            name,
+            beltColor,
+            age,
+            skills,
+            id: Math.random()
+        }
+
+        dispatch('addPerson', person)
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="name" bind:value={name}>
+    <input type="text" placeholder="belt color" bind:value={beltColor}>
+    <input type="number" placeholder="age" bind:value={age}>
+    <label for="form">Skills:</label>
+    <input type="checkbox" bind:group={skills} value="fighting"> Fighting <br>
+    <input type="checkbox" bind:group={skills} value="sleeping"> Sleeping <br>
+    <input type="checkbox" bind:group={skills} value="running"> Running <br>
+    <label for="form">Belt Color</label>
+    <select bind:value={beltColor}>
+        <option value="black">black</option>
+        <option value="white">white</option>
+        <option value="orange">orange</option>
+        <option value="blue">blue</option>
+    </select>
+    <button>Add Person</button>
+</form>
+
+
+
+
+<style>
+
+</style>
---
+++ practice1/src/main.js
@@ -0,0 +1,10 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'big cock dev'
+	}
+});
+
+export default app;
